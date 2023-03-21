76262ee3afecef00bfd3e5ec0953b3db659c8608
Jonathan Contreras <43402047+JonathanBContreras@users.noreply.github.com>
2023-03-20 20:36:13 -0700

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -103,11 +103,11 @@ def test_update_account(self):
 
     # update the account
     new_account = resp.get_json()
-        new_account["name"] = "Something Known"
-        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        updated_account = resp.get_json()
-        self.assertEqual(updated_account["name"], "Something Known")
+    new_account["name"] = "Something Known"
+    resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+    self.assertEqual(resp.status_code, status.HTTP_200_OK)
+    updated_account = resp.get_json()
+    self.assertEqual(updated_account["name"], "Something Known")
 
 
 ######################################################################
