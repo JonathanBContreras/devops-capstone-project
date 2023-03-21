ae6e6b099cb7d4127f8b08327a41c4a5a9283eed
Jonathan Contreras <43402047+JonathanBContreras@users.noreply.github.com>
2023-03-20 22:12:00 -0700

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,8 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
+
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -77,6 +79,8 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -95,6 +99,8 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
